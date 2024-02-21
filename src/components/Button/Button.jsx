a775c0a7103e27bb7baa741fa8d050b253364390
PerflexEr <stepasorokin56@gmail.com>
2024-02-21 18:18:53 +0200

    some components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+ import './Button.css'
+
+ import React from 'react';
+ 
+ const Button = (props) => {
+  return (
+    <button {...props} className={'button ' + props.className}> 
+      
+    </button>
+  );
+ };
+ 
+ export default Button;
