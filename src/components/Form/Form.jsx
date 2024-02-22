3633f508dc03244a746dab793deb35e6d245d8b2
PerflexEr <stepasorokin56@gmail.com>
2024-02-22 12:25:02 +0200

    some fixes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,82 +1,77 @@
-import './Form.css'
+import React, {useCallback, useEffect, useState} from 'react';
+import './Form.css';
+import {useTelegram} from "../../hooks/useTelegram";
 
-import React, { useState , useEffect, useCallback } from 'react';
-import { useTelegram } from '../../hooks/useTelegram';
 const Form = () => {
-
   const [country, setCountry] = useState('');
   const [street, setStreet] = useState('');
   const [subject, setSubject] = useState('physical');
-  
   const {tg} = useTelegram();
 
-  useEffect(() => {
-    tg.MainButton.setParams({
-        text: 'Отправить данные'
-    });
-  }, [tg.MainButton]);
-
-  useEffect(() => {
-    if(!street || !country) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.show();
-    }
-  }, [tg.MainButton, country, street]);
-
   const onSendData = useCallback(() => {
-    const data = {
-      country,
-      street,
-      subject
-    };
-    tg.sendData(JSON.stringify(data));
-  }, [country, street, subject, tg]);
-
+      const data = {
+          country,
+          street,
+          subject
+      }
+      tg.sendData(JSON.stringify(data));
+  }, [country, street, subject])
 
   useEffect(() => {
-    tg.WebApp.onEvent('mainButtonClicked' , onSendData);
-    return () => {
-      tg.WebApp.offEvent('mainButtonClicked' , onSendData);
-    };
-  }, [onSendData, tg.WebApp]);
-  
-  
+      tg.onEvent('mainButtonClicked', onSendData)
+      return () => {
+          tg.offEvent('mainButtonClicked', onSendData)
+      }
+  }, [onSendData])
+
+  useEffect(() => {
+      tg.MainButton.setParams({
+          text: 'Отправить данные'
+      })
+  }, [])
+
+  useEffect(() => {
+      if(!street || !country) {
+          tg.MainButton.hide();
+      } else {
+          tg.MainButton.show();
+      }
+  }, [country, street])
+
   const onChangeCountry = (e) => {
-    setCountry(e.target.value)
+      setCountry(e.target.value)
   }
 
   const onChangeStreet = (e) => {
-    setStreet(e.target.value)
+      setStreet(e.target.value)
   }
 
   const onChangeSubject = (e) => {
-    setSubject(e.target.value)
+      setSubject(e.target.value)
   }
 
-
   return (
-    <div className={"form"}>
-      <h3>Введите ваши данные</h3>
-      <input
-        className={'input'}
-        type="text"
-        placeholder={'Страна'}
-        value={country}
-        onChange={onChangeCountry}
-      />
-      <input
-        className={'input'}
-        type="text"
-        placeholder={'Улица'}
-        value={street}
-        onChange={onChangeStreet}
-      />
-      <select value={subject} onChange={onChangeSubject} className={'select'}>
-        <option value={'physical'}>Физ. лицо</option>
-        <option value={'legal'}>Юр. лицо</option>
-      </select>
-  </div>
+      <div className={"form"}>
+          <h3>Введите ваши данные</h3>
+          <input
+              className={'input'}
+              type="text"
+              placeholder={'Страна'}
+              value={country}
+              onChange={onChangeCountry}
+          />
+          <input
+              className={'input'}
+              type="text"
+              placeholder={'Улица'}
+              value={street}
+              onChange={onChangeStreet}
+          />
+          <select value={subject} onChange={onChangeSubject} className={'select'}>
+              <option value={'physical'}>Физ. лицо</option>
+              <option value={'legal'}>Юр. лицо</option>
+          </select>
+      </div>
   );
 };
 
